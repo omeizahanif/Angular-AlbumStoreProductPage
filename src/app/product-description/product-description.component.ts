a378f7e582b537ba99cf8899d75bf5219a865eb9
Hanif Adams <omeizahanif@gmail.com>
2019-11-01 18:31:21 +0000

    inject ProductService instance

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,10 +7,14 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-
+  albumInfo: any;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response => {
+      this.albumInfo = response;
+      console.log(this.albumInfo);
+    });
   }
 
 }
