04f9ec6b19398dd6e93803816a37adff53e2f3e5
Hanif Adams <omeizahanif@gmail.com>
2019-11-01 19:00:39 +0000

    import Observable and add type

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -9,7 +11,7 @@ export class ProductService {
   
   private _albumUrl: string = '../assets/album.json';
     
-  getAlbum(id: number) {
+  getAlbum(id: number): Observable<Album> {
        return this._http.get(this._albumUrl)
        .map(response => response.json());
     }
